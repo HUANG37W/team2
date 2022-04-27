81b44ed5a0701d354e7baf2dc835f12b918dbf19
robertdbjohnson <joh16151@byui.edu>
2022-04-27 17:32:58 -0600

    Added my name to team.txt

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,30 +3,18 @@ module.exports = {
     // ['@snowpack/plugin-sass' ],
   ],
   mount: {
-<<<<<<< HEAD
-    src: '/',
-  },
-  buildOptions: {
-    out: 'build',
-    baseUrl: '/sleepoutside/build/',
-=======
     src: "/",
   },
   buildOptions: {
     out: "build",
     baseUrl: "/sleepoutside/build/",
->>>>>>> 60772a768836857a10d8c54547b1784c585f2850
     clean: true,
   },
   experiments: {
     optimize: {
       // 'bundle': true,
       minify: true,
-<<<<<<< HEAD
-      target: 'es2015',
-=======
       target: "es2015",
->>>>>>> 60772a768836857a10d8c54547b1784c585f2850
     },
   },
 };
